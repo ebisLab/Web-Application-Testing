5949c5f7886dd892442579c8113ab5e9809a815d
ebisLab <ebaiden@techie.com>
2019-08-09 08:40:42 -0400

    tested function to expect striked button when clicked to be true. it was set to false

--- baseball/src/components/Dashboard.test.js
+++ baseball/src/components/Dashboard.test.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each'; //cleans up after each test, because not getting double rendering.
 import Dashboard from './Dashboard';
+import { exportAllDeclaration } from '@babel/types';
 
 describe('<Dashboard>', () => {
     it('renders without crashing', () => {
         render (<Dashboard />);
     });
     it('strike works', () => {
-        const {getByText} = render (<Dashboard/>);
+        let clicked = false;
+        const {getByText} = render (<Dashboard strike={() => clicked = true}/>);
         const strikeBtn = getByText(/strike/i)
         //Clicking lets us interact with DOM elements
+        fireEvent.click(strikeBtn);
+        expect(clicked).toBe(true);
     });
 })
