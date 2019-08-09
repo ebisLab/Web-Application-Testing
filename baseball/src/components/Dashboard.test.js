2428bac9b08b44e29310938a7f2418574dead818
ebisLab <ebaiden@techie.com>
2019-08-09 08:38:09 -0400

    fireEvent imported to help us interact with DOM elements

--- baseball/src/components/Dashboard.test.js
+++ baseball/src/components/Dashboard.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each'; //cleans up after each test, because not getting double rendering.
 import Dashboard from './Dashboard';
-import { ENETUNREACH } from 'constants';
 
 describe('<Dashboard>', () => {
     it('renders without crashing', () => {
         render (<Dashboard />);
     });
-    it('strike work', () => {
+    it('strike works', () => {
         const {getByText} = render (<Dashboard/>);
-        // const {getByText} = display //display is an object so you can distructure like so
-        getByText(/strike/i)
+        const strikeBtn = getByText(/strike/i)
+        //Clicking lets us interact with DOM elements
     });
 })
