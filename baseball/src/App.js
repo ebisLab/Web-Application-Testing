0784c093e3771b9bb129432364be1debf316dc6d
ebisLab <ebaiden@techie.com>
2019-08-09 09:08:46 -0400

    ball logic set up

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -7,10 +7,36 @@ import Dashboard from './components/Dashboard'
 function App() {
   const [strikes, setStrikes] = useState(0);
   const [balls, setBalls] = useState(0);
+  const scoreStrike = () => {
+    if (strikes === 2) {
+      setStrikes(0);
+      setBalls(0);
+    } else {
+      setStrikes (strikes + 1);
+    }
+    
+  };
+  const scoreBalls = () => {
+    if (balls === 3) {
+      setStrikes(0);
+      setBalls(0);
+    } else {
+      setBalls (balls + 1);
+    }
+  };
+  const scoreFoul = () => {
+    if (strikes !== 2) {
+      setStrikes(strikes + 1);
+    }
+  };
+  const scoreHit = () => {
+    setStrikes (0);
+    setBalls (0);
+  };
   return (
     <div className="App">
       <Display strikes={strikes} balls={balls}/>
-      <Dashboard />
+      <Dashboard strike={scoreStrike} ball={scoreBalls} foul={scoreFoul} hit={scoreHit}/>
     </div>
   );
 }
