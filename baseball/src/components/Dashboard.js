27b083918d9db3062bc30d50417980fa4fdac5c5
ebisLab <ebaiden@techie.com>
2019-08-09 07:10:47 -0400

    created components. Checked to see if tests passed on App.test.js

---
+++ baseball/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Dashboard(props){
+    return (
+        <div>
+            <h1>Dashboard Section</h1>
+            <button>Strike</button>
+            <button>Ball</button>
+            <button>Foul</button>
+            <button>Hit</button>
+            
+            </div>
+    );
+};
+
+export default Dashboard;
---
+++ baseball/src/components/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './Dashboard'
+
+function Display(props){
+    return (
+        <>
+        <h1>Dispaly Section</h1>
+        <Dashboard />
+        </>
+    );
+};
+
+export default Display;
